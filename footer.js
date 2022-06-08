2d84a3fadc79ff18b9a2cf0096495bfe9c91c577
dristy20 <86736563+dristy20@users.noreply.github.com>
2022-06-08 20:50:10 +0530

    Add files via upload

---
+++ footer.js
@@ -0,0 +1,15 @@
+const createFooter=()=>{
+    let footer=document.querySelector('footer');
+     footer.innerHTML=`<div class="footer-content">
+     <img src="logo.jpg" class="logo">
+     <div class="footer-container">
+      <p>Thank You For Visitng Our Site</p>
+      <p>For Any Query Contact Us: <i>2078-167839</i></p>
+      <p>Official Email: <i>musfiy2067@gmail.com</i></p>
+     </div>
+   </div>
+    <p class="footer-title">About Us</p>
+    <p class="info">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+  `;
+}
+ createFooter();
